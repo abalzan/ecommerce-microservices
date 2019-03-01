0037e1b0de969266cd7ec85763193cd2100b3e48
abalzan <andreibalzan@gmail.com>
2019-03-01 05:42:06 +0000

    using events (Product Event Listener and Product Event) in controller

--- product-api/src/main/java/com/andrei/product/controller/ProductController.java
+++ product-api/src/main/java/com/andrei/product/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.andrei.product.controller;
 
+import com.andrei.product.event.ProductEvent;
+import com.andrei.product.exception.Http404Exception;
 import com.andrei.product.model.Product;
 import com.andrei.product.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
@@ -7,11 +9,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @Slf4j
 @RestController
-public class ProductController {
+public class ProductController extends AbstractController{
 	private final ProductService  productService;
 
 	public ProductController(ProductService productService) {
@@ -21,31 +21,47 @@ public class ProductController {
 	@PostMapping("/products")
 	public ResponseEntity<?> createProduct(@RequestBody Product product) {
 		Product savedProduct = productService.save(product);
+
+		ProductEvent productCreatedEvent = new ProductEvent("One Product created", savedProduct);
+		applicationEventPublisher.publishEvent(productCreatedEvent);
+
 		return ResponseEntity.ok().body("New Product has been saved with ID:" + savedProduct.getId());
 	}
 
 	@GetMapping("/products/{id}")
 	public ResponseEntity<Product> getProduct(@PathVariable("id") long id) {
-		Product product = Optional.ofNullable(productService.getProduct(id)).orElseThrow(() -> new RuntimeException("Product not found!")).get();
+		Product product = productService.getProduct(id).orElseThrow(() -> new Http404Exception("Product not found!"));
+
+		ProductEvent productRetrievedEvent = new ProductEvent("Product retrieved", product);
+		applicationEventPublisher.publishEvent(productRetrievedEvent);
+
 		return ResponseEntity.ok().body(product);
 	}
 
 	@GetMapping("/products")
 	public @ResponseBody Page<Product> getProductsByPage(
-			@RequestParam(value="pagenumber", defaultValue="0") Integer pageNumber,
-			@RequestParam(value="pagesize", defaultValue="20") Integer pageSize) {
+			@RequestParam(value="pagenumber", defaultValue=DEFAULT_PAGE_NUMBER) Integer pageNumber,
+			@RequestParam(value="pagesize", defaultValue=DEFAULT_PAGE_SIZE) Integer pageSize) {
 
 		return productService.getProductByPage(pageNumber, pageSize);
 	}
 
 	@PutMapping("/products/{id}")
 	public ResponseEntity<?> updateProduct(@PathVariable("id") long id, @RequestBody Product product) {
-		productService.save(product);
+        productService.getProduct(id).orElseThrow(() -> new Http404Exception("Product not found!"));
+
+        productService.save(product);
+
+		ProductEvent productUpdatedEvent = new ProductEvent("Product updated", product);
+		applicationEventPublisher.publishEvent(productUpdatedEvent);
+
 		return ResponseEntity.ok().body("Product has been updated successfully.");
 	}
 
 	@DeleteMapping("/products/{id}")
 	public ResponseEntity<?> deleteProduct(@PathVariable("id") long id) {
+        productService.getProduct(id).orElseThrow(() -> new Http404Exception("Product not found!"));
+
 		productService.deleteProduct(id);
 		return ResponseEntity.ok().body("Product has been deleted successfully.");
 	}
