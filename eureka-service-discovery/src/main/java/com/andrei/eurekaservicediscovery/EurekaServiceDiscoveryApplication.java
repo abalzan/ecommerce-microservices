b75b0afe4efaea9f9b91f425bdc3a85835765739
abalzan <andreibalzan@gmail.com>
2019-03-02 19:53:00 +0000

    first commit

---
+++ eureka-service-discovery/src/main/java/com/andrei/eurekaservicediscovery/EurekaServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.andrei.eurekaservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceDiscoveryApplication.class, args);
+	}
+
+}
