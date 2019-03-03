1f8db09631b7188182ccbfbcc5e28ee6d4af86f1
abalzan <andreibalzan@gmail.com>
2019-03-03 14:09:29 +0000

    first commit

---
+++ remote-config-service/src/main/java/com/andrei/remoteconfigservice/RemoteConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.andrei.remoteconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteConfigServiceApplication.class, args);
+	}
+
+}
