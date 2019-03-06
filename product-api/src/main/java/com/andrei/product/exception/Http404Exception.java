8c0ed3cb16e29fe907dc43db125c03b3f5d581c1
abalzan <andreibalzan@gmail.com>
2019-03-06 21:08:11 +0000

    fixing response status from exception classes

--- product-api/src/main/java/com/andrei/product/exception/Http404Exception.java
+++ product-api/src/main/java/com/andrei/product/exception/Http404Exception.java
@@ -1,5 +1,9 @@
 package com.andrei.product.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class Http404Exception extends RuntimeException {
 
     public Http404Exception() {
