2542239953d4b8f0c2ef71f368e97c2f17394fb5
abalzan <andreibalzan@gmail.com>
2019-03-06 21:09:15 +0000

    Configuring feign client to call category endpoint (category-api) thought product-api

---
+++ product-api/src/main/java/com/andrei/product/feign/CategoryFeignClient.java
@@ -0,0 +1,19 @@
+package com.andrei.product.feign;
+
+import com.andrei.product.model.Category;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/*
+ * Name needs to match with application-name configured on bootstrap.yml,
+ * this annotation basically say: Feign Client go to service discovery and fetch al the endpoints
+ * that belongs to category-api
+ */
+@FeignClient(name = "category-api")
+public interface CategoryFeignClient {
+
+    @GetMapping("/categories/{id}")
+    ResponseEntity<Category> getCategory(@PathVariable("id") long id) ;
+}
--- product-api/src/main/java/com/andrei/product/service/ProductService.java
+++ product-api/src/main/java/com/andrei/product/service/ProductService.java
@@ -1,8 +1,12 @@
 package com.andrei.product.service;
 
+import com.andrei.product.exception.Http400Exception;
 import com.andrei.product.exception.Http404Exception;
+import com.andrei.product.feign.CategoryFeignClient;
+import com.andrei.product.model.Category;
 import com.andrei.product.model.Product;
 import com.andrei.product.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -11,17 +15,20 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final CategoryFeignClient categoryFeignClient;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, CategoryFeignClient categoryFeignClient) {
         this.productRepository = productRepository;
+        this.categoryFeignClient = categoryFeignClient;
     }
 
     public Product save(Product product) {
-        return productRepository.save(product);
+        return productRepository.save(validateProductCategories(product));
     }
 
     public Page<Product> getProductByPage(Integer pageNumber, Integer pageSize) {
@@ -38,5 +45,26 @@ public class ProductService {
 
     public void deleteProduct(Long productId) {
         productRepository.deleteById(productId);
+    }
+
+    private Product validateProductCategories(Product product) {
+
+        Optional.ofNullable(product.getCategory())
+                .map(this::validateCategory).orElseThrow(() -> new Http400Exception("Category does not exists"));
+
+        Optional.ofNullable(product.getParentCategory())
+                .map(this::validateCategory)
+                .orElseThrow(() -> new Http400Exception("Category does not exists"));
+
+        return product;
+    }
+
+    private Category validateCategory(Category category) {
+        try {
+            return categoryFeignClient.getCategory(category.getId()).getBody();
+        } catch (Exception e) {
+            log.error("Category {} is invalid", category.getId(), e);
+            throw new Http400Exception("Invalid Category");
+        }
     }
 }
