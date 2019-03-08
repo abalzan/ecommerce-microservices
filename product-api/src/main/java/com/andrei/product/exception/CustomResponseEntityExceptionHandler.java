c647b6ffe46e915c8bd22874e34b7a33f3788050
abalzan <andreibalzan@gmail.com>
2019-03-08 07:08:27 +0000

    creating generic error for handling

--- product-api/src/main/java/com/andrei/product/exception/CustomResponseEntityExceptionHandler.java
+++ product-api/src/main/java/com/andrei/product/exception/CustomResponseEntityExceptionHandler.java
@@ -18,6 +18,7 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
 
     private Counter http400CounterException = Metrics.counter("com.andrei.controller.HTTP400");
     private Counter http404CounterException = Metrics.counter("com.andrei.controller.HTTP404");
+    private Counter httpGenericCounterException = Metrics.counter("com.andrei.controller.Exception");
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(Http400Exception.class)
@@ -36,6 +37,16 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
 
         log.info("Resource not found Exception {} ", exception.getLocalizedMessage());
         http404CounterException.increment();
+        return new RestAPIException(new Date(), exception.getMessage(), "Requested resource not found");
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public @ResponseBody
+    RestAPIException handleRuntimeException(Exception exception, WebRequest request, HttpServletResponse response) {
+
+        log.info("Resource not found Exception {} ", exception.getLocalizedMessage());
+        httpGenericCounterException.increment();
         return new RestAPIException(new Date(), exception.getMessage(), "Requested resource not found");
     }
 
