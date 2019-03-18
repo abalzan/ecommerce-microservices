e84ff8cc68d3f3321902bf11f5d39ee097180dc1
abalzan <andreibalzan@gmail.com>
2019-03-18 11:35:52 +0000

    implementing AOP for category-api

--- category-api/src/main/java/com/andrei/category/CategoryApiApplication.java
+++ category-api/src/main/java/com/andrei/category/CategoryApiApplication.java
@@ -3,8 +3,10 @@ package com.andrei.category;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 @EnableDiscoveryClient
 public class CategoryApiApplication {
 
--- category-api/src/main/java/com/andrei/category/aop/RestControllerAspect.java
+++ category-api/src/main/java/com/andrei/category/aop/RestControllerAspect.java
@@ -3,9 +3,8 @@ package com.andrei.category.aop;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Metrics;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -13,17 +12,52 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class RestControllerAspect {
 
-    private Counter counter = Metrics.counter("com.andrei.category.created");
+    private Counter counterControllerCreateSuccessful = Metrics.counter("com.andrei.category.controller.create");
+    private Counter counterControllerUpdateSuccessful = Metrics.counter("com.andrei.category.controller.update");
+    private Counter counterControllerDeleteSuccessful = Metrics.counter("com.andrei.category.controller.delete");
 
-    @Before("execution(public * com.andrei.category.controller.*Controller.*(..))")
-    public void generallAllMethodsAspect(){
-      log.info("calling aop");
+    private Counter counterControllerCalls = Metrics.counter("com.andrei.category.controller.execution");
+    private Counter counterControllerException = Metrics.counter("com.andrei.category.controller.exception");
+
+    @Pointcut("execution(public * com.andrei.category.controller.CategoryController.*(..))")
+    private void allMethodsCategoryControllerClass() {
     }
 
-
-    @AfterReturning("execution(public * com.andrei.category.controller.*Controller.createCategory(..))")
-    public void getCalledOnCategorySave(){
-        log.info("incrementing saved category");
-        counter.increment();
+    @Before("allMethodsCategoryControllerClass()")
+    public void generallAllMethodsAspect(JoinPoint joinPoint) {
+        log.info("Calling method(s) {} with parameters {}", joinPoint.getSignature(), joinPoint.getArgs());
     }
+
+    @Before("allMethodsCategoryControllerClass()")
+    public void getCalledCategoryOperation() {
+        log.info("incrementing calls to categoryController");
+        counterControllerCalls.increment();
+    }
+
+    @AfterReturning(value = "execution(public * com.andrei.category.controller.CategoryController.createCategory(..))")
+    public void getCalledOnCategorySave() {
+        log.info("Incrementing statistics create category");
+        counterControllerCreateSuccessful.increment();
+    }
+
+    @AfterReturning(value = "execution(public * com.andrei.category.controller.CategoryController.updateCategory(..))")
+    public void getCalledOnCategoryUpdate() {
+        log.info("Incrementing statistics update category");
+        counterControllerUpdateSuccessful.increment();
+    }
+
+    @AfterReturning(value = "execution(public * com.andrei.category.controller.CategoryController.deleteCategory(..))")
+    public void getCalledOnCategoryDelete() {
+        log.info("Incrementing statistics delete category");
+        counterControllerDeleteSuccessful.increment();
+    }
+
+    @AfterThrowing(value = "allMethodsCategoryControllerClass()", throwing = "categoryException")
+    public void getCalledOnCategoryException(JoinPoint joinPoint, Throwable categoryException) {
+        log.info("Incrementing statistics exception for category");
+        counterControllerException.increment();
+
+        log.error("Method {}, with arguments {} Throws the exception", joinPoint.getSignature(), joinPoint.getArgs(), categoryException);
+    }
+
 }
