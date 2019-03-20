4fabfd9effbec64822d8c03c65867ee950728840
abalzan <andreibalzan@gmail.com>
2019-03-20 21:36:37 +0000

    adding Validation on endpoints

--- product-api/src/main/java/com/andrei/product/controller/ProductController.java
+++ product-api/src/main/java/com/andrei/product/controller/ProductController.java
@@ -4,24 +4,36 @@ import com.andrei.product.event.ProductEvent;
 import com.andrei.product.exception.ExceptionConstants;
 import com.andrei.product.model.Product;
 import com.andrei.product.service.ProductService;
+import com.andrei.product.service.ValidationErrorService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
 
 @Slf4j
 @RestController
 public class ProductController extends AbstractController{
-	private final ProductService  productService;
 
-	public ProductController(ProductService productService) {
+	private final ProductService  productService;
+	private final ValidationErrorService validationErrorService;
+
+	public ProductController(ProductService productService, ValidationErrorService validationErrorService) {
 		this.productService = productService;
+		this.validationErrorService = validationErrorService;
 	}
 
 	@PostMapping("/products")
-	public ResponseEntity<?> createProduct(@RequestBody Product product) {
+	public ResponseEntity<?> createProduct(@Valid @RequestBody Product product, BindingResult result) {
+
+		ResponseEntity<?> responseEntity = validationErrorService.validationErrorService(result);
+
+		if (responseEntity != null) return responseEntity;
+
 		Product savedProduct = productService.save(product);
 
 		ProductEvent productCreatedEvent = new ProductEvent("One Product created", savedProduct);
@@ -49,10 +61,15 @@ public class ProductController extends AbstractController{
 	}
 
 	@PutMapping("/products/{id}")
-	public ResponseEntity<?> updateProduct(@PathVariable("id") long id, @RequestBody Product product) {
-        productService.getProduct(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found!"));
+	public ResponseEntity<?> updateProduct(@Valid @PathVariable("id") long id, @RequestBody Product product, BindingResult result) {
 
-        productService.save(product);
+		ResponseEntity<?> responseEntity = validationErrorService.validationErrorService(result);
+
+		if (responseEntity != null) return responseEntity;
+
+		productService.getProduct(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found!"));
+
+		productService.save(product);
 
 		ProductEvent productUpdatedEvent = new ProductEvent("Product updated", product);
 		applicationEventPublisher.publishEvent(productUpdatedEvent);
