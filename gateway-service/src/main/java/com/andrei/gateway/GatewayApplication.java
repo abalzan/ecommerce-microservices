2c78bf4b112f1de453c285fa767343af0c23ea2b
abalzan <andreibalzan@gmail.com>
2019-06-22 20:10:59 +0100

    updating docker files
    Docker compose is on version 2, mainly because healthcheck parameter
    which is not present on docker 3.

--- gateway-service/src/main/java/com/andrei/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/com/andrei/gateway/GatewayApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -14,4 +15,29 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+    @Bean
+    public ZuulPreFilter preFilter() {
+        return new ZuulPreFilter();
+    }
+
+    @Bean
+    public ZuulPostFilter postFilter() {
+        return new ZuulPostFilter();
+    }
+
+    @Bean
+    public ZuulErrorFilter errorFilter() {
+        return new ZuulErrorFilter();
+    }
+
+    @Bean
+    public ZuulRouteFilter routeFilter() {
+        return new ZuulRouteFilter();
+    }
+
+
+    @Bean
+    public ZuulLoggingFilter loggingFilter() {
+        return new ZuulLoggingFilter();
+    }
 }
